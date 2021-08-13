13bba6925e34ac37ec5704af77443c1dfce5ef30
andotant <ralinirinaandoniaina@gmail.com>
2021-08-13 12:24:29 +0300

    deuxieme commit

---
+++ myapp/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Form from "./components/Form";
+import "./App.css";
+//import Footer from './components/Footer';
+
+const App = () => {
+  return (
+    <div>
+      <Form/> 
+    </div>
+  );
+};
+
+export default App;
---
+++ myapp/src/components/SignupFormSuccess.js
@@ -0,0 +1,101 @@
+import React from "react";
+import "../App.css";
+
+// <div className="app-wrapper">
+//<h1 className="form-success">Account Created</h1>
+//</div>
+const SignupFormSuccess = () => {
+    return (
+        <div>
+            <header className="l-header">
+                <nav className="nav bd-grid">
+                    <div>
+                        <a href className="nav__logo">E-Pandemic.</a>
+                    </div>
+                    <div className="nav__menu">
+                        <ul className="nav__list">
+                            <li className="nav__item">Home</li>
+                            <li className="nav__item">About</li>
+                            <li className="nav__item">Message</li>
+                            <li className="nav__item">Profile</li>
+                        </ul>
+                    </div>
+                </nav>
+            </header>
+            <div className="conteneur">
+                <div className="contenu1">
+                    <div className="sary1"></div>
+                    <div className="article1">
+                         <p className="p"> AstraZeneca-Covishield = AstraZeneca-Vaxzevria <br></br>
+                         💁‍♀️Le Saviez-Vous?  <br></br>
+                         ✅#AstraZeneca-#Covishield est le nom donné au vaccin 💉contre la #COVID19 fabriqué par le Serum Institute of India.<br></br>
+                         ➡️C’est exactement le même vaccin 💉que l’AstraZeneca-Vaxzevria fabriqué ailleurs.  <br></br>
+                         ✅ Ils offrent le même niveau de #protection contre les formes graves de la maladie et le décès dû à la #COVID19. <br></br>
+                            Source : <a className="aA2"> https://web.facebook.com/OMSMadagascar/posts/1715384578651930</a>
+                        </p>
+                    </div>
+                </div>
+
+                <div className="contenu2">
+                    <div className="sary2"></div>
+                    <div className="article2">
+                         <p className="p">🤔Qu'apporte la politilisatoin de l'origine du covid-19 dans la lutte mondiale contre l'épidémie? <br></br>
+                         Selon les données publiées par l'Organisation mondiale de la santé (OMS), au 5 Août, le nombre cumulé de nouveaux caas confirmés de COVID-19 dans le monde dépassait 200 millions et le nombre cumulé de décès, 4,25 millions. <br></br>
+                         Source : <a className="aA2">htpps://china.cn/3jAPgqK</a><br></br>
+                         <a className="aA2">China.org.cn</a>
+                        </p>
+                    </div>
+                </div>
+
+                <div className="contenu3">
+                    <div className="sary3"></div>
+                    <div className="article3">
+                         <p className="p">
+                         📣PREVENTION - Arrivée à Ivato des vaccins Johnson and Johnson ce mardi après midi
+                        Antananarivo, 27 Juillet, 6h20 - Comme annoncé par le Premier ministre Christian Ntsay, les vaccins Janssen des laboratoires Johnson and Johnson, don des États Unis, sont attendus ce mardi à Antananarivo. <br></br>Leur arrivée est prévue dans l'après-midi à l'aéroport international d'Ivato. Dimanche, le chef du gouvernement a parlé de plus de 300.000 doses si les annonces initiales ont indiqué 165.000 doses.<br></br>
+                        ⭕Comme l'AstraZeneca, le vaccin Janssen utilise aussi la technologie du vecteur viral et se conserve à des températures de 2° à 8°C. Par contre, il s'administre en une seule dose. Il fait partie des quatre vaccins recommandés par l’Académie nationale de médecine de Madagascar.
+                        <br></br>Retrouvez l'actualité à Madagascar sur notre site web <a className="aA2">www.2424.mg</a>
+                        <br></br>Source : <a className="aA2">https://web.facebook.com/2424.mg/posts/1468903960152035</a>
+                         </p>
+                    </div>
+                </div>
+
+                <div className="contenu4">
+                    <div className="sary4"></div>
+                    <div className="article4">
+                         <p className="p"> Grâce à un don des États-Unis d'un demi-million de doses de vaccins contre la COVID-19, Haïti démarre sa campagne de vaccination.<br></br> 
+                            « Maintenant que je suis vaccinée, je me sens beaucoup plus en sécurité pour exercer mon métier et protéger les enfants et leurs parents » Alabrie, infirmière. 
+                            <br></br>Plus les agents de santé, enseignants et travailleurs sociaux doivent attendre avant d’être vaccinés, plus le risque est grand pour leur santé et la santé, la sécurité et le potentiel des enfants.
+                            <br></br>Les pays riches doivent partager les vaccins disponibles dans le cadre de COVAX, programme mondial qui garantit l’accès équitable de tous aux vaccins.<br></br>
+                            Source : <a className="aA2">https://web.facebook.com/unicefenfrancais/</a>
+                        </p>
+                    </div>
+                </div>
+
+                <div className="contenu5">
+                    <div className="sary5"></div>
+                    <div className="article5">
+                         <p className="p"> Voici les faits concernant le vaccin AstraZeneca #Covishield contre la COVID-19. 
+                        <br></br>✅ AstraZeneca-Covishield et les autres vaccins #AstraZeneca sont les mêmes. Ils offrent le même niveau de protection contre les formes graves de la maladie et le décès dû à la #COVID19.
+                        <br></br>✅ AstraZeneca-Covishield a été homologué pour une utilisation en situation d'urgence par l'OMS <a className="aA2">(World Health Organization (WHO))</a>.<br></br>
+                        Source : <a className="aA2">https://web.facebook.com/OMSRegionAfrique/posts/351926179719046</a>
+                        </p>
+                    </div>
+                </div>
+
+            </div>
+            <footer className="footer">
+                <p className="footertext">
+                    <em>Created by RALINIRINA Andoniaina Tantely, IGGLIA_5 | copyright 10 August 2021</em>
+                    <ul className="contact">
+                        <li>Facebook : <a className="contact1"> Ando Ralinirina</a></li>
+                        <li>E-mail : <a className="contact1">ralinirinaandoniaina@gmail.com</a></li>
+                        <li>Telephone : <a className="contact1">+261 34 93 966 49</a></li>
+                    </ul>
+                </p>
+            </footer>
+        </div>
+    )
+}
+
+export default SignupFormSuccess;
